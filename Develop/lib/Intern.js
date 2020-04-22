239194ee664c52cdeed1d18682b6efe0dbab6f14
Joy Chen <joychen5069@gmail.com>
2020-04-22 13:03:35 -0400

    added questions for Intern

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const inquirer = require("inquirer")
+
+//start asking questions here
+async function InternQuest() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Intern's name?"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What is the Intern's ID number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Intern's email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Where does the Intern go to school?"
+        },
+    ])
+}
+
+InternQuest();
