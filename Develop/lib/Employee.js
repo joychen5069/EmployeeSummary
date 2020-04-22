4f83df16cc3054e35bf50b02f4decfc4ae2f775e
Joy Chen <joychen5069@gmail.com>
2020-04-22 13:02:21 -0400

    added questions for engineer

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,4 @@
 // TODO: Write code to define and export the Employee class
+const engineer = require("./Engineer.js")
+const intern = require("./Intern.js")
+const manager = require("./Manager")
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,37 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const inquirer = require("inquirer")
+
+//start asking questions here
+async function EngineerQuest() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Engineer's name?"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What is the Engineer's ID number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the Engineer's GitHub Username?"
+        },
+    ])
+}
+
+EngineerQuest();
+//need to ask for ID#
+
+//need to ask for email
+
+//need to ask for GitHub Username
+
+//export out
