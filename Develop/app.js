c522e6f814d434b3cec29d1b29ec3950925c6476
Joy Chen <joychen5069@gmail.com>
2020-04-27 12:25:43 -0400

    renamed prompt to app.js and created stylesheet

--- Develop/app.js
+++ Develop/app.js
@@ -1,35 +1,196 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 const path = require("path");
 const fs = require("fs");
 
+
+//create const that log to html
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/htmlRenderer")
 
-const render = require("./lib/htmlRenderer");
+const write = () => {
+    console.log(team)
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(team), "utf-8");
+    
+}
+
+const team = []
+
+const additional = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What role would you like to create for your employee?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Done"
+
+            ]
+        }
+    ])
+}
+
+//Manager
+async function createManager() {
+    inquirer.prompt([
+        //ask for their name
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Manger's name?"
+        },
+
+        //ask for their ID number
+        {
+            type: "input",
+            name: "Id",
+            message: "What is the Manager's ID number?"
+        },
+
+        //ask for their email
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Manager's email?"
+        },
+        //ask for their office number
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the Manager's office number?"
+        }
+    ])
+        .then(await function (answers) {
+            //create manager and add to team
+            const manager = new Manager(answers.name, answers.Id, answers.email, answers.officeNumber)
+            team.push(manager)
+            // console.log(manager)
+            
+            //ask for another employee
+            init();
+
+        })
+}
+
+//Engineer
+async function createEngineer() {
+    inquirer.prompt([
+        //ask for their name
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Engineer's name?"
+        },
+
+        //ask for their ID number
+        {
+            type: "input",
+            name: "Id",
+            message: "What is the Engineer's ID number?"
+        },
+
+        //ask for their email
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Engineer's email?"
+        },
+        //ask for their office number
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is the Engineer's gitHub username?"
+        },
+
+    ])
+        .then(await function (answers) {
+            //push engineer to the team
+            const engineer = new Engineer(answers.name, answers.Id, answers.email, answers.gitHub)
+            team.push(engineer)
+
+            init();
+
+        })
+}
+
+//Intern
+async function createIntern() {
+    inquirer.prompt([
+        //ask for their name
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Intern's name?"
+        },
+
+        //ask for their ID number
+        {
+            type: "input",
+            name: "Id",
+            message: "What is the Intern's ID number?"
+        },
+
+        //ask for their email
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Intern's email?"
+        },
+
+        //ask for their school
+        {
+            type: "input",
+            name: "school",
+            message: "Where does the Intern go to school?"
+        }
+        
+    ])
+        .then(await function (answers) {
+            //push intern to team
+            const intern = new Intern(answers.name, answers.Id, answers.email, answers.school)
+            team.push(intern)
+
+                init();
+            
+        })
+}
+
+async function init() {
+    const role = await additional();
+    select(role)
+}
+
+async function select(answer) {
+    switch (answer.role) {
+
+        //create engineer case
+        case ("Engineer"):
+            console.log("engineer");
+            createEngineer()
+            break;
+
+        //create Intern case
+        case ("Intern"):
+            console.log("intern");
+            createIntern();
+            break;
+
+        default:
+            //idk what you did but it broke the code
+            console.log("Your team.html has been written")
+            write();
+            break;
+    }
+}
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+createManager();
